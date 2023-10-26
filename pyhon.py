aca57b00f85f5ee13d9cb9b3fea4238861b40b71
WhatTheBoy <boichenko.vladislavv@gmail.com>
2023-10-26 20:44:21 +0300

    if/elif/else

--- pyhon.py
+++ pyhon.py
@@ -1,58 +1,30 @@
-# print(1 < 2)
-# print(type(True))
-# print(type(False))
+# day_time = input('Enter the time of day you like:')
+# if day_time == 'morning':
+#     print('monster is going to the forest')
+# elif day_time == 'afternoon':
+#     print('monster is eating')
+# elif day_time == 'evening':
+#     print('monster is walking')
+# elif day_time == 'night':
+#     print('monster is sleeping')
+# else:
+#     print('monster is doing something')
 
-# # Copmarison operators
+integer_number = int(input('Enter an integer number'))
+if integer_number % 2 == 0:
+    print('The number is even')
+else:
+    print('The number is odd')
 
-# print(1 == 1)
-# print(1 == 2)
-# print('hello' == 'hello')
-# print('hello' == 'hi')
-
-
-# print(1 != 1)
-# print(1 != 2)
-# print('hello' != 'hello')
-# print('Hello' != 'hello')
-
-# print(1 < 2)
-# print(1 > 2)
-# print(1 <= 2)
-# print(1 >= 2)
-
-# print(ord('a'))
-# print(ord('b'))
-# print('a' > 'b')
-# print('a' < 'b')
-# print('a' <= 'b')
-# print('a' >= 'b')
-# print('hi' < 'hello')
-# print(ord('h'))
-# print(ord('i'))
-# print(ord('e'))
+# # False: empty object , None, 0
+# if 0:
+#     print('something')
+# if 1:
+#     print('something')
 
 
-# x = 10
-# y = 20
-# print(x == y)
-# print(x < y)
-# print(x > y)
-# print(x >= y)
-# print(x <= y)
-# print(x != y)
-
-# age = int(input('Input your age:'))
-# print('Access is premitted:' + str(age >= 18))
-
-x = 2
-y = 3
-print(x == y)
-print(x != y)
-print(x <= y)
-print(x >= y)
-print(x < y)
-print(x > y)
-
-
-print('g' < 'n')
-print(ord('g'), ord('n'))
+lucky_number = int(input('Enter any number:'))
+if lucky_number == 7:
+    print('7 is a lucky number! Today is your lucky day!')
+else:
+    print('Thank you! Have a nice day!')
