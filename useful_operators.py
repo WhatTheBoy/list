f322bc6fce059a476190559409b6ad1d5b26cc0b
WhatTheBoy <boichenko.vladislavv@gmail.com>
2023-10-30 19:23:19 +0200

    useful_operators

---
+++ useful_operators.py
@@ -0,0 +1,45 @@
+# for x in range(1, 100, 6):
+#     print(x)
+# print(range(x))
+# print(list(range(5)))
+
+
+# letter_index = 0
+from random import *
+my_str = 'abcdefg'
+# for letter in my_str:
+#     print(letter + ' is at index ' + str(letter_index))
+#     letter_index += 1
+
+
+# for letter, index in enumerate(my_str):
+#     print(letter)
+#     print(index)
+
+# print('a' in 'bank')
+# print('s' in 'bank')
+
+
+# letter_list = ['a', 'b', 'c', True]
+
+# print('a' in letter_list)
+# print('n' in letter_list)
+# print(True in letter_list)
+
+
+# my_dict = {
+#     'a': 1
+# }
+# print(my_dict['a'])
+
+
+print(min(1, 2, 3, 543))
+print(max(1, 2, 3, 543))
+my_list = [1, 2, 3, 543]
+print(max(my_list))
+print(max('Hello'))
+for lett in 'hello':
+    print(ord(lett))
+shuffle(my_list)
+print(my_list)
+print(randint(1, 4123))
